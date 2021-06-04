d01eebc22064db7da64bf5d9187ac5949083890d
Chris Clifton <chrisc03@hotmail.com>
2021-06-03 20:08:35 -0400

    initial commit

---
+++ app/subscribe/controller.py
@@ -0,0 +1,28 @@
+from werkzeug.utils import redirect
+from app import subscribe
+from flask import Blueprint, render_template, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+subscribe = Blueprint("subscriptions", __name__, template_folder="./templates")
+
+
+class Subscription(FlaskForm):
+    email_sms = StringField("email_sms", validators=[DataRequired()])
+
+
+@subscribe.route("/subscribe")
+def index():
+    form = Subscription()
+    if form.validate_on_submit():
+        return redirect("/success")
+    return render_template("subscribe.html", form=form)
+
+
+@subscribe.route("/unsubscribe")
+def index():
+    form = Subscription()
+    if form.validate_on_submit():
+        return redirect("/success")
+    return render_template("unsubscribe.html", form=form)
